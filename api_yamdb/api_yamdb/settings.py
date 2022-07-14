1e8adc45c7fe8737f2c13797aae098efc064ff2e
Владимир Рязанов <ryazanov745@gmail.com>
2022-07-14 19:55:34 +0300

    update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='x+5t+*4$+le4+vyn8c%+kipd!8yxc6-!o3
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.100.202']
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
